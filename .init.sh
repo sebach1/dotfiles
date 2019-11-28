af3bcfa108f0cf6c8e80b3aeb25b9218a1fd3570
Sebastián Chamena <sebachamena@gmail.com>
2019-11-28 09:26:58 -0300

    Correct workspaces assign

--- .init.sh
+++ .init.sh
@@ -1,8 +1,8 @@
 #!bin/sh
 sleep 3
-i3-msg 'workspace 1:Chr; exec /usr/bin/google-chrome-stable --force-device-scale-factor=0.85'
-i3-msg 'workspace 2:Sh; exec /usr/bin/urxvt'
-i3-msg 'workspace 3:Vs; exec /usr/bin/code'
-i3-msg 'workspace 4:Sck; exec /usr/bin/slack'
-i3-msg 'workspace 10:Music; exec /usr/bin/spotify'
+i3-msg 'workspace 1; exec /usr/bin/google-chrome --force-device-scale-factor=0.85'
+i3-msg 'workspace 2; exec /usr/bin/urxvt'
+i3-msg 'workspace 3; exec /usr/bin/code'
+i3-msg 'workspace 4; exec /usr/bin/slack'
+i3-msg 'workspace 10; exec /usr/bin/spotify'
 
